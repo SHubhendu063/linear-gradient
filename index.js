e5f3132292a031604ccd54c421f04ff29e5d7ea6
Shubhendu Khuntia <shubhendu.khuntia678@gmail.com>
2024-12-23 09:34:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 let button1 = document.querySelector('#btn1');
 let button2= document.querySelector('#btn2');
 let mainContainer = document.querySelector('.maincontainer');
-// let ele=document.querySelector('#copytext');
+let ele=document.querySelector('#copytext');
 
-
-let color1="";
-let color2="";
+let color1="#63e65f";
+let color2="#00b3ff";
 
 
 function hexaValue(){
@@ -22,19 +21,21 @@ function randomGradient1(){
     color1=hexaValue();
     button1.textContent=color1;
     button1.style.background=color1;
-    mainContainer.style.background=`linear-gradient(to right top,${color1}, ${color2})`;
+    mainContainer.style.backgroundImage=`linear-gradient(to right top,${color1}, ${color2})`;
+    ele.textContent=`background-color:linear-gradient(to right top,${color1}, ${color2});`;
 };
 
 function randomGradient2(){
     color2=hexaValue();    
     button2.textContent=color2;
     button2.style.background=color2;
-    mainContainer.style.background=`linear-gradient(to right top,${color1}, ${color2})`;
+    mainContainer.style.backgroundImage=`linear-gradient(to right top,${color1}, ${color2})`;
+    ele.textContent=`background-color:linear-gradient(to right top,${color1}, ${color2});`;
 };
+console.log(color1);
+console.log(color2);
+
 
 
 button1.addEventListener('click',randomGradient1);
 button2.addEventListener('click',randomGradient2);
-
-// ele.textContent=`linear-gradient(to right top,${color1}, ${color2})`;
-// console.log(color1);
